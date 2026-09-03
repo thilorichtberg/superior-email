6203a6a08b5d11c53c9867174f1faf39ed068162
Thilo Richtberg <thilo.richtberg@tum.de>
2026-09-03 02:25:40 +0200

    chore: initialize Spring Boot skeleton

---
+++ src/main/java/com/thilorichtberg/superior_email/SuperiorEmailApplication.java
@@ -0,0 +1,13 @@
+package com.thilorichtberg.superior_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperiorEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperiorEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thilorichtberg/superior_email/SuperiorEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilorichtberg.superior_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperiorEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
